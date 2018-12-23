b7b796ae3676c0d6495f1915f738b50f151340f4
Elizabeth Cannon <elizabeth230@gmail.com>
2018-12-23 11:38:03 -0800

    starting with alreadystrings and booleans

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,7 +4,20 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  //mission: take in an obj and add each character to a string, return entire string
-  //base case: no more elements in obj to add to string
-  //remove the element you just added to your string from the obj
+  // mission: take in an obj and add each character to a string, return entire string
+  // base case: no more elements in obj to add to string
+  // remove the element you just added to your string from the obj
+
+  // if it's a string already, return the string (base case?)
+  if (typeof(obj) === 'string') {
+  	return obj;
+  } 
+  // if it's a boolean use toString and then return the string
+  else if (typeof(obj) === 'boolean') {
+  	obj = obj.toString();
+  	stringifyJSON(obj);
+  }
+  // if it's an array create a string that starts with a [ then add each element of the array and end with another ] 
+  // if it's an object create a string that starts with a { then add each key/value pair from the obj and end with another }
+
 };
