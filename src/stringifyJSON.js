abe5f49e5c3592d151e67586e8c95bdc624c2379
Elizabeth Cannon <elizabeth230@gmail.com>
2018-12-23 12:04:41 -0800

    adding more psuedocode

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -18,6 +18,24 @@ var stringifyJSON = function(obj) {
   	stringifyJSON(obj);
   }
   // if it's an array create a string that starts with a [ then add each element of the array and end with another ] 
-  // if it's an object create a string that starts with a { then add each key/value pair from the obj and end with another }
+    // arr = ['foo', true]
+    // we want ", " to separate each element in the array
+    // string = "["
+    // add array[0]
+    // add ", "
+    // add array[1]
+    // add ", "
+    // etc.
 
+  // if it's an object create a string that starts with a { then add each key/value pair from the obj and end with another }
+    // obj = {'foo': true, 'bar': false, 'baz': null}
+    // Object.entries(obj) = [["foo", true], ["bar", false], ["baz", null]]
+      // this breaks the obj into an array of key value pair arrays - can then use the step specified for an array?
+    // we want ": " to separate each element in the inner array and then ", " to separate in the outer array
+    // string = "{"
+    // add array[0][0]
+    // add ": "
+    // add array[0][1]
+    // add ", "
+    // etc.
 };
