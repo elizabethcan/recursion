b81ce6c9261f1a09c1a258f117e6f38d53b76e48
Elizabeth Cannon <elizabeth230@gmail.com>
2018-12-23 17:26:58 -0800

    making each part of the function look more similar so i can figure out how to use recursion

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -9,18 +9,23 @@ var stringifyJSON = function(obj) {
   // remove the element you just added to your string from the obj
 
   // if it's a string already, return the string (base case?)
+  var str = '';
   if (typeof(obj) === 'string') {
   	return obj;
   } 
   // if it's a number use toString and then return the string
   else if (typeof(obj) === 'number') {
-    obj = obj.toString();
-    stringifyJSON(obj);
+    function(el) {
+      str += el;
+    }
+    return str;
   }
   // if it's a boolean use toString and then return the string
   else if (typeof(obj) === 'boolean') {
-  	obj = obj.toString();
-  	stringifyJSON(obj);
+  	function(el) {
+      str += el;
+    }
+    return str;
   }
   // if it's an array create a string that starts with a [ then add each element of the array and end with another ] 
     // arr = ['foo', true]
@@ -34,7 +39,7 @@ var stringifyJSON = function(obj) {
     // remove last ", "
     // add "]"
     else if (Array.isArray(obj)) {
-      var str = '[';
+      var str += '[';
       obj.forEach(function(el){
         if(typeof(el) === 'string') {
           str += `"${el}",`;
@@ -49,13 +54,9 @@ var stringifyJSON = function(obj) {
 
   // if it's an object create a string that starts with a { then add each key/value pair from the obj and end with another }
     // obj = {'foo': true, 'bar': false, 'baz': null}
-    // Object.entries(obj) = [["foo", true], ["bar", false], ["baz", null]]
-      // this breaks the obj into an array of key value pair arrays - can then use the step specified for an array?
-    // we want ": " to separate each element in the inner array and then ", " to separate in the outer array
+    // we want ": " to separate each key/value in the inner array and then "," to separate each pair
     // string = "{"
-    // add array[0][0]
-    // add ": "
-    // add array[0][1]
+    // add key: value
     // add ", "
     // etc.
 
