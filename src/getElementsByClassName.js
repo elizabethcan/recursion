981c35514929e0759ff34aec1e7dd8eb4f69930e
Elizabeth Cannon <elizabeth230@gmail.com>
2018-12-27 15:13:49 -0800

    isolated the block which should be replaced by the recursive function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,53 +5,26 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className, currentElement) {
-  // output: returns an array-like object of all child elements which have all of the given class names
-
-  // document.body selects the whole body
-  // element.classList is a read-only property that returns a live DOMTokenList collection of the class attributes of the element.
-  // node.childNodes read-only property returns a live NodeList of child nodes of the given element where the first child node is assigned index 0.
-
-  // starting from outermost layer of document check document.body for elements with desired class name
-    // var body = document.body
-        // check if body.classList.contains(className), if so, push this element to the array (result.push(body))
-    // var elementsArr = body.children
-        // if elementsArr.length > 0
-            // for each element in the array
-
-
-  // var body = document.body
-  // body.classList will give an array of all the classes associated with the body element
-  // elementsArr = body.children will give an array of all the elements in the body
-  // elementsArr[i] will give an array of the class names associated with that element
-  // check each element of this array for a match with the input class
-    // if an element matches, push elementsArr[i] to resultArr
-
   var result = [];
-  if (!currentElement) {
-    var currentElement = currentElement || document.body;
-    if (currentElement.classList.contains(className)) {
+  var currentElement = currentElement || document.body;
+  if (currentElement.classList.contains(className)) {
       result.push(currentElement);
-    }
   }
-  // this is the part i want to repeat for every layer of elements
   if (currentElement.children.length > 0) {
-    currentElement = currentElement.children;
-    Array.prototype.forEach.call(currentElement, function(el) {
-      // the line below appears on line 31-32
+    Array.prototype.forEach.call(currentElement.children, function(el) {
+
+      //getElementsByClassName(className, el)
+      //this block is what can be cut out with recursion
       if(el.classList.contains(className)) {
         result.push(el);
       }
       if(el.children.length > 0) {
-        currentElement = el.children;
-        Array.prototype.forEach.call(currentElement, function(el) {
-          // the line below appears on line 31-32
+        Array.prototype.forEach.call(el.children, function(el) {
           if(el.classList.contains(className)) {
             result.push(el);
           }
           if(el.children.length > 0) {
-            currentElement = el.children;
-            Array.prototype.forEach.call(currentElement, function(el) {
-              // the line below appears on line 31-32
+            Array.prototype.forEach.call(el.children, function(el) {
               if(el.classList.contains(className)) {
                 result.push(el);
               }
@@ -59,11 +32,9 @@ var getElementsByClassName = function(className, currentElement) {
           }
         });
       }
-      //getElementsByClassName(className, el);
+
+
     });
   }
-  
-  console.log("result:");
-  console.log(result);
   return result;
 };
